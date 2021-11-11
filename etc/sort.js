e90552dfe78c63c393e3599dd9c1ce1ed488b906
jinseoIT <69947833+jinseoIT@users.noreply.github.com>
2021-11-11 11:41:30 +0900

    :zap: sort 정리

--- etc/sort.js
+++ etc/sort.js
@@ -5,12 +5,13 @@
 첫 번째 인수가 두 번째 인수보다 클 경우 + 값 
 */
 const array = [1, 5, 12, 17, 27, 31, 2, 13, 99, 30];
-console.log("default", array.sort());
+
+console.log("default", array.sort()); // default, [1, 12, 13, 17,  2, 27, 30, 31,  5, 99]
 console.log(
   "asc",
-  array.sort((a, b) => a - b)
+  array.sort((a, b) => a - b) // asc, [1,  2,  5, 12, 13,17, 27, 30, 31, 99]
 );
 console.log(
   "desc",
-  array.sort((a, b) => b - a)
+  array.sort((a, b) => b - a) // desc,[99, 31, 30, 27, 17, 13, 12,  5,  2,  1]
 );
