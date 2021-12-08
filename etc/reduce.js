fa46e5cf1e645f4ceddee09734cc61b148f4a222
jinseoIT <69947833+jinseoIT@users.noreply.github.com>
2021-12-08 21:41:27 +0900

    :sparkles: 방금 그곡 추가

---
+++ etc/reduce.js
@@ -0,0 +1,165 @@
+/* 
+MDN - https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce
+reduce() 메서드는 배열의 각 요소에 대해 주어진 리듀서(reducer) 함수를 실행하고, 하나의 결과값을 반환합니다.
+
+리듀서 함수는 네 개의 인자를 가집니다.
+누산기 (acc)
+현재 값 (cur)
+현재 인덱스 (idx)
+원본 배열 (src)
+리듀서 함수의 반환 값은 누산기에 할당되고, 누산기는 순회 중 유지되므로 결국 최종 결과는 하나의 값이 됩니다.
+*/
+
+const array1 = [1, 2, 3, 4];
+const reduce = (previousValue, currentValue) => previousValue + currentValue;
+
+// 1 + 2 + 3 + 4
+console.log(array1.reduce(reduce));
+// expected output: 10
+
+// 5 + 1 + 2 + 3 + 4
+console.log(array1.reduce(reduce, 5));
+// expected output: 15
+
+/* 객체 배열에서의 값 합산 */
+const initialValue = 0;
+const sum = [{ x: 1 }, { x: 2 }, { x: 3 }].reduce(
+  (accumulator, currentValue) => accumulator + currentValue.x,
+  initialValue
+);
+
+console.log(sum); // logs 6
+
+/* 중첩 배열 펼치기 */
+const flattened = [
+  [0, 1],
+  [2, 3],
+  [4, 5],
+].reduce((accumulator, currentValue) => accumulator.concat(currentValue), []);
+
+/* 객체 내의 값 인스턴스 개수 세기 */
+const names = ["Alice", "Bob", "Tiff", "Bruce", "Alice"];
+
+const countedNames = names.reduce(function (allNames, name) {
+  if (name in allNames) {
+    allNames[name]++;
+  } else {
+    allNames[name] = 1;
+  }
+  return allNames;
+}, {});
+// countedNames is:
+// { 'Alice': 2, 'Bob': 1, 'Tiff': 1, 'Bruce': 1 }
+
+/* 속성으로 객체 분류하기 */
+const people = [
+  { name: "Alice", age: 21 },
+  { name: "Max", age: 20 },
+  { name: "Jane", age: 20 },
+];
+
+function groupBy(objectArray, property) {
+  return objectArray.reduce(function (acc, obj) {
+    var key = obj[property];
+    if (!acc[key]) {
+      acc[key] = [];
+    }
+    acc[key].push(obj);
+    return acc;
+  }, {});
+}
+
+var groupedPeople = groupBy(people, "age");
+// groupedPeople is:
+// {
+//   20: [
+//     { name: 'Max', age: 20 },
+//     { name: 'Jane', age: 20 }
+//   ],
+//   21: [{ name: 'Alice', age: 21 }]
+// }
+
+/* 확장 연산자와 초기값을 이용하여 객체로 이루어진 배열에 담긴 배열 연결하기 */
+var friends = [
+  {
+    name: "Anna",
+    books: ["Bible", "Harry Potter"],
+    age: 21,
+  },
+  {
+    name: "Bob",
+    books: ["War and peace", "Romeo and Juliet"],
+    age: 26,
+  },
+  {
+    name: "Alice",
+    books: ["The Lord of the Rings", "The Shining"],
+    age: 18,
+  },
+];
+
+var allbooks = friends.reduce(
+  function (accumulator, currentValue) {
+    return [...accumulator, ...currentValue.books];
+  },
+  ["Alphabet"]
+);
+
+// allbooks = [
+//   'Alphabet', 'Bible', 'Harry Potter', 'War and peace',
+//   'Romeo and Juliet', 'The Lord of the Rings',
+//   'The Shining'
+// ]
+
+/*  배열의 중복 항목 제거 
+참고: Set과 Array.from()을 사용할 수 있는 환경이라면, 
+let orderedArray = Array.from(new Set(myArray));
+를 사용해 중복 요소를 제거할 수도 있습니다.
+*/
+let arr = [1, 2, 1, 2, 3, 5, 4, 5, 3, 4, 4, 4, 4];
+let result = arr.sort().reduce((accumulator, current) => {
+  const length = accumulator.length;
+  if (length === 0 || accumulator[length - 1] !== current) {
+    accumulator.push(current);
+  }
+  return accumulator;
+}, []);
+console.log(result); //[1,2,3,4,5]
+
+/* 함수 구성을 위한 파이프 함수 */
+// Building-blocks to use for composition
+const double = (x) => x + x;
+const triple = (x) => 3 * x;
+const quadruple = (x) => 4 * x;
+
+// Function composition enabling pipe functionality
+const pipe =
+  (...functions) =>
+  (input) =>
+    functions.reduce((acc, fn) => fn(acc), input);
+
+// Composed functions for multiplication of specific values
+const multiply6 = pipe(double, triple);
+const multiply9 = pipe(triple, triple);
+const multiply16 = pipe(quadruple, quadruple);
+const multiply24 = pipe(double, triple, quadruple);
+
+// Usage
+multiply6(6); // 36
+multiply9(9); // 81
+multiply16(16); // 256
+multiply24(10); // 240
+
+/* reduce()로 map() 작성 */
+if (!Array.prototype.mapUsingReduce) {
+  Array.prototype.mapUsingReduce = function (callback, thisArg) {
+    return this.reduce(function (mappedArray, currentValue, index, array) {
+      mappedArray[index] = callback.call(thisArg, currentValue, index, array);
+      return mappedArray;
+    }, []);
+  };
+}
+
+[1, 2, , 3].mapUsingReduce(
+  (currentValue, index, array) => currentValue + index + array.length
+); // [5, 7, , 10]
